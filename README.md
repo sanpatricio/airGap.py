8aeef380d17297a91ba160e19bed5d531666a47a
sanpatricio <patrickriggs@gmail.com>
2016-09-30 21:02:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# nomiGrate
+# version 0.9
+# By Patrick Riggs (github: sanpatricio)
+
+nomiGrate assists you with nominating for migration (get it?) files that may be ready for copying between two air-gapped systems or networks by recursively looking through a directory for files modified within N days ago, then offering a couple ways to proceed with the list of nomiGrated files (display on screen, write to log file, copy itentified files to a directory)
---
+++ nomiGrate.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+import os
+import datetime as dt
+import sys
+from shutil import copyfile
+from time import sleep
+
+now       = dt.datetime.now()
+searchDir = raw_input("In which directory do I begin my search: ")
+ago       = input("How many days back am I looking: ")
+shellAgo  = now-dt.timedelta(days=ago)
+exclude   = set([".git"])
+hits      = {}
+
+def writeOut(results):
+	out = ""
+	for key in sorted(results):
+		out += results[key] + ":" + str(key) + str("\n")
+
+	return out
+
+
+def copyFiles(results,dest):
+	if not os.path.exists(dest) or not os.path.isdir(dest):
+		os.mkdir(dest)
+
+	for key in sorted(results):
+		print "Moving " + key + " to " + dest + "/" + os.path.basename(key)
+		try:
+			copyfile(key, dest+"/"+os.path.basename(key))
+		except StandardError as errStr:
+			print "Error: Couldn't copy " + str(key) + "; system feedback: " + str(errStr)
+
+	return "...finished"
+
+
+print "Seaerching for files modified since " + str(shellAgo)
+
+for root,dirs,files, in os.walk(searchDir):
+	dirs[:] = [d for d in dirs if d not in exclude]
+	for fname in files:
+		path = os.path.join(root,fname)
+		st = os.stat(path)
+		mtime = dt.datetime.fromtimestamp(st.st_mtime)
+		if mtime > shellAgo:
+			hits[path] = mtime.strftime('%Y-%m-%d %H:%M')
+
+
+if len(hits) != 0:
+	stamp = now.strftime('%Y%m%d-%H%M%S-')
+	report = stamp + "nomiGrate-Report.txt"
+	directory = stamp + "nominatedForMigration"
+
+	print "Found " + str(len(hits)) + " modified files in the last " + str(ago) + " day(s)."
+	print "Actions:"
+	print "[1] Nothing, return to command line."
+	print "[2] Collect copies of his in ./" + directory + "/"
+	print "[3] Write these results to ./" + report
+	print "[4] Write these results to the screen"
+
+	action = int(raw_input("What should I do with them [1]: ") or 1)
+
+	if action == 2:
+		print "Copying to ./" + directory + "/..."
+		print copyFiles(hits,directory)
+
+	elif action == 3:
+		print "Writing to " + report
+		filename = "./" + report
+		target = open(filename, 'w')
+		output = writeOut(hits)
+		target.write(output)
+		target.close()
+		print "...done."
+
+	elif action == 4:
+		print writeOut(hits)
+
+	else:
+		print "Goodbye"
+
+else:
+	print "No files modified in the last " + str(ago) + " day(s)."
